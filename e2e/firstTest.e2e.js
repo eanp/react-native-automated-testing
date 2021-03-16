89bea9b7b3ea1b0a5f6c354f00f118d3ac985a0a
ean <eanpraha@gmail.com>
2021-03-16 18:17:31 +0700

    detox

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,52 @@
+// describe('Example', () => {
+//   beforeAll(async () => {
+//     await device.launchApp();
+//   });
+
+//   beforeEach(async () => {
+//     await device.reloadReactNative();
+//   });
+
+//   it('should have welcome screen', async () => {
+//     await expect(element(by.id('welcome'))).toBeVisible();
+//   });
+
+//   it('should show hello screen after tap', async () => {
+//     await element(by.id('hello_button')).tap();
+//     await expect(element(by.text('Hello!!!'))).toBeVisible();
+//   });
+
+//   it('should show world screen after tap', async () => {
+//     await element(by.id('world_button')).tap();
+//     await expect(element(by.text('World!!!'))).toBeVisible();
+//   });
+// });
+
+// describe('Login flow test', () => {
+//   beforeEach(async () => {
+//     await device.reloadReactNative();
+//   });
+
+//   it('should have login screen', async () => {
+//     await expect(element(by.id('loginView'))).toBeVisible();
+//   });
+
+//   it('should fill login form', async () => {
+//     await element(by.id('usernameInput')).typeText('varunk');
+//     await element(by.id('passwordInput')).typeText('test123\n');
+//     await element(by.id('loginButton')).tap();
+//   });
+
+//   it('should show dashboard screen', async () => {
+//     await expect(element(by.id('dashboardView'))).toBeVisible();
+//     await expect(element(by.id('loginView'))).toNotExist();
+//   });
+// });
+
+describe('Login flow test', () => {
+  it('should fill login form', async () => {
+    await element(by.id('usernameInput')).typeText('varunk');
+    await element(by.id('passwordInput')).typeText('test123\n');
+    await element(by.id('loginButton')).tap();
+  });
+});
